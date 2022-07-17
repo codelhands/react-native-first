7715af04775e9f4ec6c61493aa6546a09d515742
Elif <elffemre@gmail.com>
2022-07-17 16:13:40 +0300

    v.0.1.3 - button oluşturma
    -on press metodu kullanılmalı
    -title kullanılmalı

--- SeniSeviyorum/App.js
+++ SeniSeviyorum/App.js
@@ -1,14 +1,24 @@
 import React from 'react';
-import {View, Text, SafeAreaView} from 'react-native';
+import {View, Text, SafeAreaView, Button} from 'react-native';
 
-function App() {
+const App = () => {
   return (
+    //stringlerde süslü paranteze gerek yok
+    // control space ile component
+    // hiyerarşi yapısı viewin çocuğu text'tir.
     <SafeAreaView>
       <View>
-        <Text>Deneme</Text>
+        <Text>Hello World</Text>
       </View>
+      <Text>Hello World 2</Text>
+      <Button
+        title="Press Me!"
+        onPress={() => {
+          null;
+        }}
+      />
     </SafeAreaView>
   );
-}
+};
 
 export default App;
