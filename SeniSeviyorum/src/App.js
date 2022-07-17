8c4b6fc8b130747fac0c0c5a81aa785439cb2c47
Elif <elffemre@gmail.com>
2022-07-17 18:25:27 +0300

    v.0.1.9 - otamatikleştirme
    card yapısını otamatikleştirdik
    props yapısını öğrendik

--- SeniSeviyorum/src/App.js
+++ SeniSeviyorum/src/App.js
@@ -11,37 +11,8 @@ import Card from './components/Card';
 const App = () => {
   return (
     <SafeAreaView style={styles.container}>
-      <View style={styles.card_container}>
-        <View style={styles.card_body}>
-          <Text style={styles.card_title}>Eddar Stark</Text>
-          <Text style={styles.card_text}>Winter is coming...</Text>
-        </View>
-        <TouchableOpacity style={styles.card_button_container}>
-          <Text style={styles.card_button_title}>I LIKED</Text>
-        </TouchableOpacity>
-      </View>
-      <View style={styles.card_container}>
-        <View style={styles.card_body}>
-          <Text style={styles.card_title}>Elif Sarı Emre</Text>
-          <Text style={styles.card_text}>Ben bunun altından kalkamam</Text>
-        </View>
-        <TouchableOpacity style={styles.card_button_container}>
-          <Text style={styles.card_button_title}>I LIKED</Text>
-        </TouchableOpacity>
-      </View>
-      <View style={styles.card_container}>
-        <View style={styles.card_body}>
-          <Text style={styles.card_title}>Yakup Sarı</Text>
-          <Text style={styles.card_text}>
-            Kim diyo uyuşturucu kullanıyorum diye
-          </Text>
-        </View>
-        <TouchableOpacity style={styles.card_button_container}>
-          <Text style={styles.card_button_title}>I LIKED</Text>
-        </TouchableOpacity>
-      </View>
-
-      <Card />
+      <Card title="Yakup Sarı" text="haaa" />
+      <Card title="Elif Sarı" text="Yakubumu çok seviyorum" />
     </SafeAreaView>
   );
 };
@@ -51,39 +22,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#e0e0e0',
-  },
-  card_container: {
-    backgroundColor: 'white',
-    margin: 10,
-    borderWidth: 0.1,
-    borderColor: 'gray',
-    borderRadius: 10,
-  },
-  card_body: {
-    padding: 10,
-  },
-  card_title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    margin: 10,
-    marginBottom: 3,
-  },
-  card_text: {
-    fontSize: 10,
-    margin: 10,
-    marginTop: 3,
-  },
-  card_button_container: {
-    backgroundColor: 'aqua',
-    padding: 10,
-    alignItems: 'center',
-    borderBottomLeftRadius: 10,
-    borderBottomRightRadius: 10,
-  },
-  card_button_title: {
-    fontSize: 10,
-    fontWeight: 'bold',
-    color: 'white',
   },
 });
 
--- SeniSeviyorum/src/components/Card/Card.js
+++ SeniSeviyorum/src/components/Card/Card.js
@@ -2,12 +2,12 @@ import React from 'react';
 import {Text, TouchableOpacity, View} from 'react-native';
 import deneme from './Card.style'; //STYLE DOSYASINI importladık
 
-const Card = () => {
+const Card = props => {
   return (
     <View style={deneme.container}>
       <View style={deneme.body}>
-        <Text style={deneme.title}>John Doe</Text>
-        <Text style={deneme.text}>Lorem Ipsum...</Text>
+        <Text style={deneme.title}>{props.title}</Text>
+        <Text style={deneme.text}>{props.text}</Text>
       </View>
       <TouchableOpacity style={deneme.button_container}>
         <Text style={deneme.button_title}>I LIKED</Text>
